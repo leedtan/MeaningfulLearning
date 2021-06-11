e77113a4d9c4775f5470f7097d8c215276b8611c
Yechan <leyekkan@gmail.com>
2021-06-11 19:06:45 -0400

    HaveSomeBug

--- Week 8/data.py
+++ Week 8/data.py
@@ -25,10 +25,10 @@ def picture(df):
 
 def dataShift(df, numeric_cols):
     df['Date'] = df.index
-    for days_past in range(1, 14):
+    for days_past in range(1, 7):
         df['prev_date' + str(days_past)] = df['Date'] - days_past#pd.DateOffset(days=days_past)
 
-    for days_past in range(1, 14):
+    for days_past in range(1, 7):
         df_past = df[numeric_cols + ['Date']].set_index('Date')
         df_past.columns = [c + '_past' + str(days_past) for c in df_past.columns]
         df = df.set_index('prev_date' + str(days_past)).join(df_past).reset_index(drop=True)
@@ -37,14 +37,14 @@ def dataShift(df, numeric_cols):
     return df
 
 def get_x_y(numeric_cols, cleaner):
-    past_cols = [[c + '_past' + str(days_past) for c in numeric_cols] for days_past in range(1, 14)]
+    past_cols = [[c + '_past' + str(days_past) for c in numeric_cols] for days_past in range(1, 7)]
     past_cols = [c for cols in past_cols for c in cols]
     x = cleaner[past_cols]
     y = cleaner[numeric_cols]
     return x, y, past_cols
 
 def get_mdl_inputs(df, numeric_cols, past_cols):
-    y_diff = (df['High'] + df['Low']).iloc[1:].values/2 - df['Close'].iloc[:-1].values
+    y_diff = ((df['High'] + df['Low']).iloc[1:].values/2 - df['Close'].iloc[:-1].values)/df['Close'].iloc[:-1].values
     x_with_current = df[numeric_cols + past_cols].iloc[:-1]
     return y_diff, x_with_current
 
@@ -54,9 +54,11 @@ def calc_y(df):
     y_diff = (y_future - y_past)/y_past
     return y_diff
 
-def get_trn_val(x, numeric_cols, trn_percent = 0.8):
+def get_trn_val(x, numeric_cols, trn_percent = 0.8, val_percent = 0.2):
     n_date = x.shape[0]
-    x_trn, x_val = x.iloc[:np.floor(n_date * trn_percent).astype(int)+1], x.iloc[np.floor(n_date * trn_percent).astype(int):]
+    num_trn = np.floor(n_date * trn_percent).astype(int)
+    num_val = np.floor(n_date * val_percent).astype(int)
+    x_trn, x_val = x.iloc[:num_trn+1], x.iloc[num_trn:num_trn+num_val]
     y_trn, y_val = [calc_y(x_set) for x_set in (x_trn, x_val)]
     mean_vals = x_trn[numeric_cols].mean().mean()
     x_trn = x_trn / mean_vals
